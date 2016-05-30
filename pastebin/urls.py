40fe186f4c994b0b800afd62eaac85315029ed9b
shenry <shenry@handycodejob.com>
2016-05-30 12:09:08 -0700

    fix urls for tests and add install req file

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -5,7 +5,9 @@ from pastebin.views import index, paste_details, plain, html
 
 urlpatterns = [
     url(r'^$', index, name='djpaste_index'),
-    url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html')),
+    url(r'^help/$',
+        TemplateView.as_view(template_name='djpaste/help.html'),
+        name='djpaste_help'),
     url(r'^paste/(?P<id>\d+)/$', paste_details, name='djpaste_paste_details'),
     url(r'^plain/(?P<id>\d+)/$', plain, name='djpaste_plain'),
     url(r'^html/(?P<id>\d+)/$', html, name='djpaste_html'),
