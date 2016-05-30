96e86f8ed57f71d71c48d0b7d3127abdebf27a6a
shenry <shenry@handycodejob.com>
2016-05-30 11:46:52 -0700

    update urls.py to work in django==1.10

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -1,19 +1,18 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
 from django.views.generic import TemplateView
+from pastebin.views import index, paste_details, plain, html
 
-urlpatterns = patterns('pastebin.views',
-        url(r'^$', 'index', name='djpaste_index'),
-        url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html'), 
-        									name='djpaste_help'),
-        url(r'^paste/(?P<id>\d+)/$', 'paste_details', name='djpaste_paste_details'),
-        url(r'^plain/(?P<id>\d+)/$', 'plain', name='djpaste_plain'),
-        url(r'^html/(?P<id>\d+)/$', 'html', name='djpaste_html'),
+urlpatterns = [
+    url(r'^$', index, name='djpaste_index'),
+    url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html')),
+    url(r'^paste/(?P<id>\d+)/$', paste_details, name='djpaste_paste_details'),
+    url(r'^plain/(?P<id>\d+)/$', plain, name='djpaste_plain'),
+    url(r'^html/(?P<id>\d+)/$', html, name='djpaste_html'),
+]
 
-
-)
-
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-    							{'document_root': settings.STATIC_ROOT}),
-)
+urlpatterns += [
+    url(r'^static/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+]
